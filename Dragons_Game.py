57f0a41d57f883fbe741358af59d488994169c8a
SemKilyaev <darkvanted@gmail.com>
2019-11-05 12:54:31 +0300

    Initial commit

---
+++ Dragons_Game.py
@@ -0,0 +1,44 @@
+import random
+import time
+def displayIntro():
+    print('''Вы находитесь в землях населенных драконами.
+Перед собой вы видите две пещеры. В одной из них дружелюбный дракон,
+Который готов поделиться с вами своими сокровищами. Во второй -
+жадный и голодный дракон, который мигом вас съест''')
+    print()
+
+def chooseCave():
+    cave=''
+    while cave !='1' and cave !='2':
+        print('В какую пещеру вы войдете? (Нажмите клавишу 1 или 2)')
+        cave=input()
+
+    return cave
+
+def checkCave(chosenCave):
+    print('Вы приближаетесь к пещере')
+    time.sleep(2)
+    print('Ее темнота заставляет вас дрожать от страха...')
+    time.sleep(2)
+    print('Большой дракон выпрыгивает перед вами! Он раскрывает пасть и...')
+    print()
+    time.sleep(2)
+
+    friendlyCave=random.randint(1, 2)
+
+    if chosenCave == str(friendlyCave):
+        print('...делится с вами своими сокровищами!')
+    else:
+        print('...моментально вас съедает!')
+
+playAgain='да'
+while playAgain =='да' or playAgain =='д' or playAgain =='Да' or playAgain =='ДА':
+         
+    displayIntro()
+    caveNumber=chooseCave()
+    checkCave(caveNumber)
+
+    print('Попытаете удачу снова?) (да или нет)')
+    playAgain=input()
+
+  
